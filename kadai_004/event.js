e6142dc90c4190da488fe08192b79eb462c2ca8a
TAKAO-001 <168104465+TAKAO-001@users.noreply.github.com>
2024-10-20 09:05:28 +0900

    kadai_004のアップロード

--- kadai_004/event.js
+++ kadai_004/event.js
@@ -1,10 +1,8 @@
-$(function() {
-      $(window).on({
-        'load':()=>{
-          console.log('loadイベントが発生しました');
-        },
-        'scroll':()=>{
-          console.log('scrollイベントが発生しました');
-        },
-      });
-  });
+
+$(window).on('load',()=> {
+  console.log('loadイベントが発生しました'); 
+});
+$(window).on('scroll',()=> {
+  console.log('scrollイベントが発生しました');
+});
+      
